5dd7caa9bca326a91f9e3712157b6a7180846097
Luis Felipe Restrepo <codingnow404@gmail.com>
2026-02-26 00:25:42 -0500

    feat: implement CRUD methods in taskService

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -1,5 +1,10 @@
-import {Injectable} from '@angular/core';
-import {Task} from '../models/task.model';
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model';
+
+/*
+    Servicio encargado de gestionar la persistencia de las tareas mediante LocalStorage
+    Creo métodos para obtnener, guardar, agregar eliminar y actualizar el estado de las Tasks
+*/
 
 @Injectable({
     providedIn: 'root',
@@ -8,13 +13,45 @@ import {Task} from '../models/task.model';
 export class TaskService {
     private STORAGE_KEY = 'tasks';
 
+    //Obtengo el estado actual del localstorage y lo parseo a objetos Task, si no hay nada devuelvo un vacio
     getTasks(): Task[] {
         const data = localStorage.getItem(this.STORAGE_KEY);
         return data ? JSON.parse(data) : [];
 
     }
-
-    saveTasks(tasks: Task[]):void {
+    //Uso el localStorage para que los datos persistan al recargar la page
+    saveTasks(tasks: Task[]): void {
         localStorage.setItem(this.STORAGE_KEY, JSON.stringify(tasks));
+    }
+
+    addTask(title: string): void {
+        const tasks = this.getTasks();
+        const newTask: Task = {
+            id: crypto.randomUUID(), //Genero un codigo id unico seguro
+            title,
+            completed: false,
+            createdAt: Date.now(),
+        }
+        tasks.push(newTask);
+        this.saveTasks(tasks);
+
+    }
+
+    deleteTask(id: string): void {
+        //uso filter para mantener la inmutabilidad del array originl y eliminar la tarea con el id encontrado
+        const tasks = this.getTasks().filter(task => task.id !== id);
+        this.saveTasks(tasks);
+    }
+
+    toggleComplete(id: string): void {
+        //LOcalizo y actualizo el estado de la Task sin mutar el resto de la lista
+        const tasks = this.getTasks();
+        for (let task of tasks) {
+            if (task.id === id) {
+                task.completed = !task.completed;
+                break;
+            }
+        }
+        this.saveTasks(tasks);
     }
 }
