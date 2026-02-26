a8010c6291b8304347211a08a9940e5219c4c89d
Luis Felipe Restrepo <codingnow404@gmail.com>
2026-02-26 10:53:09 -0500

    feat(home):add task creation and fix standalone ionic imports

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import {TaskService} from '../core/services/task.service';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton
+} from '@ionic/angular/standalone';
+
+import { TaskService } from '../core/services/task.service';
 import { Task } from '../core/models/task.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-   standalone: true,
-  imports: [IonicModule, CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton
+  ],
 })
 
 export class HomePage implements OnInit {
 
   // inicializamos con un array vacío para evitar errores de renderizado en el template
   tasks: Task[] = [];
-  constructor(private taskService: TaskService) {}
-  
+  newTaskTitle: string = '';
+
+  constructor(private taskService: TaskService) { }
+
   ngOnInit(): void {
     this.loadTasks();
   }
 
-  loadTasks() {
+  loadTasks(): void {
     this.tasks = this.taskService.getTasks();
+  }
+
+  addTask(): void {
+
+    const title = this.newTaskTitle.trim();
+
+    if (title.length > 0) {
+      this.taskService.addTask(this.newTaskTitle);
+      this.newTaskTitle = '';
+      this.loadTasks();
+    }
+    
+
   }
 }
